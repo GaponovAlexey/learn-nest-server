6629ffc5bf7365f8c29614620f0e4d5cc3a36efd
kekelberi <alexei.gaponov@gmail.com>
2022-06-24 17:50:41 +0300

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,10 @@
-import { Module } from "@nestjs/common";
-import { SequelizeModule } from "@nestjs/sequelize";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'nest-db',
-      models: [],
-      autoLoadModels: true
-    }),
-  ],
-
 })
-export class AppModule {
-
-}
+export class AppModule {}
