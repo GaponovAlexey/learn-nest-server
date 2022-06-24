e55bf26e7829891aebda0bfa9765c44f02ccf18f
kekelberi <alexei.gaponov@gmail.com>
2022-06-24 19:31:59 +0300

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,5 +6,17 @@ import { AppService } from './app.service'
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postal',
+      database: 'nest-db',
+      models: [],
+      autoLoadModels: true
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+// const start = async () => {
+//   const PORT = process.env.PORT || 5000
+//   const app = await NestFactory.create(AppModule)
+
+//   await app.listen(PORT, () => console.log(`my server start ${PORT}`))
+// }
+
+// start()
+
 const start = async () => {
-  const PORT = process.env.PORT || 5000
+  const PORT = 5000;
   const app = await NestFactory.create(AppModule)
 
-  await app.listen(PORT, () => console.log(`my server start ${PORT}`))
+  await app.listen(PORT, () => console.log(`start ${PORT}`))
 }
-
 start()
