76c6200cd26be1572dc47abfbc7e6a4dc7e1d8c3
kekelberi <alexei.gaponov@gmail.com>
2022-06-24 20:42:01 +0300

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { SequelizeModule } from '@nestjs/sequelize'
+import { User } from './users/users.model'
 import { UsersModule } from './users/users.module'
 
 @Module({
@@ -15,7 +16,7 @@ import { UsersModule } from './users/users.module'
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
