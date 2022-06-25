d76c63070c00398c7dae193eb04a170032ba3bd9
kekelberi <alexei.gaponov@gmail.com>
2022-06-25 20:45:42 +0300

    save

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,6 +6,7 @@ export class UsersService {
     const user = [
       { id: 1, title: 'hi nestjs' },
       { id: 2, title: 'hi nextjs' },
+      { id: 3, title: 'hi test' },
     ]
     return user
   }
