c3fcdec7e6de3c56f085aa072bf1390c6f69e88e
kekelberi <alexei.gaponov@gmail.com>
2022-06-25 18:19:36 +0300

    save

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 const start = async () => {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule)
-
+  app.enableCors()
   await app.listen(PORT, () => console.log(`start ${PORT}`))
 }
 start()
