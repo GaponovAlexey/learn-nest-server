0fbc26ac0fe28a34d52f43838a6f78a2acfcd0b0
kekelberi <alexei.gaponov@gmail.com>
2022-06-25 22:22:31 +0300

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { ProductsService } from './products/products.service'
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ProductsController } from './products/products.controller'
+import { ProductsModule } from './products/products.module'
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mysql://mt0qovvlqiei:pscale_pw_CjfljBwyfHvuQtMRyeIYLSTI7LdTSG2THGdtE5toRCA@av51do8uu7vi.us-east-1.psdb.cloud/dbtest?ssl={"rejectUnauthorized":true}'
+    ),
+  ],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
---
+++ src/users/users.model.ts
@@ -0,0 +1,20 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript'
+
+@Table({
+  tableName: 'users',
+})
+export class User extends Model<User> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  Id: number
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string
+}
